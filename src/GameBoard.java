76950f7564e6ea3bec4930fde0a407a706ac273f
josh <77705398+joshk777@users.noreply.github.com>
2023-12-16 00:02:51 -0500

    added get and set for player placement added tester while loop (NOT PERM) added stubs for win checking

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -2,12 +2,18 @@ public class GameBoard
 {
     private char[][] board;
 
+    /**
+     * Construct a new game board
+     */
     public GameBoard()
     {
         board = new char[3][3];
         initializeBoard();
     }
 
+    /**
+     * Initialize a blank playing board
+     */
     public void initializeBoard()
     {
         for (int i = 0; i < 3; i++)
@@ -19,6 +25,31 @@ public class GameBoard
         }
     }
 
+    /**
+     * Get the current state of the board
+     *
+     * @return The game board array
+     */
+    public char[][] getBoard()
+    {
+        return board;
+    }
+
+    /**
+     * Sets a value to a specific cell of the board
+     *
+     * @param row The row index
+     * @param col The column index
+     * @param value The value to sets
+     */
+    public void setCell(int row, int col, char value)
+    {
+        board[row][col] = value;
+    }
+
+    /**
+     * Print the state of the game board
+     */
     public void printGameBoard()
     {
         for (int i = 0; i < 3; i++)
@@ -40,5 +71,41 @@ public class GameBoard
                 System.out.println("---------");
             }
         }
+    }
+
+    public boolean checkWin()
+    {
+        //TODO: Implement this method
+        return false;
+    }
+
+    public boolean checkRowWin()
+    {
+        //TODO: Implement this method
+        return false;
+    }
+
+    public boolean checkColumnWin()
+    {
+        //TODO: Implement this method
+        return false;
+    }
+
+    public boolean checkDiagonalWin()
+    {
+        //TODO: Implement this method
+        return false;
+    }
+
+    public boolean checkTie()
+    {
+        //TODO: Implement this method
+        return false;
+    }
+
+    public boolean checkRowCol()
+    {
+        //TODO: Implement this method
+        return false;
     }
 }
--- src/TicTacToeDriver.java
+++ src/TicTacToeDriver.java
@@ -1,10 +1,47 @@
+import java.util.Scanner;
 public class TicTacToeDriver
 {
     public static void main(String[] args)
     {
         GameBoard ticTacToeBoard = new GameBoard();
+        Scanner input = new Scanner(System.in);
 
-        ticTacToeBoard.printGameBoard();
+        char currentPlayer = 'X';
 
+        //TODO: Change loop control
+        while(true)
+        {
+            ticTacToeBoard.printGameBoard();
+            int position;
+
+            System.out.println("Player: " + currentPlayer + "enter your placement (1-9): ");
+            position = input.nextInt();
+
+            int row = (position - 1) / 3;
+            int col = (position - 1) % 3;
+
+            // Check if the selected cell is empty before making a move
+            if (ticTacToeBoard.getBoard()[row][col] == ' ')
+            {
+                ticTacToeBoard.setCell(row, col, currentPlayer);
+
+
+                //TODO: Check for a win or a tie here
+
+                // Switch to the other player for the next turn
+                if (currentPlayer == 'X')
+                {
+                    currentPlayer = 'O';
+                }
+                else
+                {
+                    currentPlayer = 'X';
+                }
+            }
+            else
+            {
+                System.out.println("Cell already occupied. Try again.");
+            }
+        }
     }
 }
